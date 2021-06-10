0c2a99e70e3f31ab004c68809858d94fa3bfd276
ihfazhillah <mihfazhillah@gmail.com>
2021-06-10 17:02:31 +0700

    use viewBinding

--- app/src/main/java/com/kotlin/androidplayerme/MainActivity.kt
+++ app/src/main/java/com/kotlin/androidplayerme/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kotlin.androidplayerme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlin.androidplayerme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createButton.setOnClickListener {
+            openWebViewPage()
+        }
+    }
+
+    private fun openWebViewPage() {
     }
 }
