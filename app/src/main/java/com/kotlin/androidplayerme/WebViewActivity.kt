2ad391b1f083f5a37be6e831350518367db1aa89
ihfazhillah <mihfazhillah@gmail.com>
2021-06-11 00:00:25 +0700

    add loader

--- app/src/main/java/com/kotlin/androidplayerme/WebViewActivity.kt
+++ app/src/main/java/com/kotlin/androidplayerme/WebViewActivity.kt
@@ -1,12 +1,20 @@
 package com.kotlin.androidplayerme
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.webkit.*
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.kotlin.androidplayerme.databinding.ActivityWebViewBinding
+import java.io.File
 
 class WebViewActivity : AppCompatActivity() {
     companion object {
@@ -14,11 +22,14 @@ class WebViewActivity : AppCompatActivity() {
     }
     private lateinit var binding: ActivityWebViewBinding
     private var isCreateNew = false
+    
+    private var filePathCallback: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestPermission.launch(Manifest.permission.CAMERA)
         isCreateNew = intent.getBooleanExtra(IS_CREATE_NEW, false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -36,9 +47,57 @@ class WebViewActivity : AppCompatActivity() {
                     if (
                         CookieManager.getInstance().hasCookies()
                     ) CookieHelper(this@WebViewActivity).setUpdateState(true)
+                    binding.progressBar.visibility = View.GONE
+                    visibility = View.VISIBLE
+                }
+            }
+
+            webChromeClient = object: WebChromeClient(){
+                override fun onShowFileChooser(
+                    webView: WebView?,
+                    filePathCallback: ValueCallback<Array<Uri>>?,
+                    fileChooserParams: FileChooserParams?
+                ): Boolean {
+                    this@WebViewActivity.filePathCallback = filePathCallback
+
+                    fileChooserParams?.let {
+                        if (it.isCaptureEnabled){
+                            openFileResultContract.launch(null)
+                        } else {
+                            openDocumentContract.launch("image/*")
+                        }
+                    }
+                    return true
+                }
+
+                override fun onPermissionRequest(request: PermissionRequest?) {
+                    Log.d("PERMISSION", "onPermissionRequest: ${request?.resources} ")
+                    request?.grant(arrayOf(Manifest.permission.CAMERA))
+
                 }
             }
         }
+    }
+
+
+    private val openFileResultContract  = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){
+        val path = MediaStore.Images.Media.insertImage(contentResolver, it, "fromCamera.jpeg", "")
+        filePathCallback?.onReceiveValue(arrayOf(Uri.parse(path)))
+    }
+
+    private val openDocumentContract = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){
+        filePathCallback?.onReceiveValue(arrayOf(it))
+    }
+
+    private val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){ granted ->
+        if (!granted){
+            Toast.makeText(this, "Camera permission not granted.", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 
@@ -72,11 +131,16 @@ class WebViewActivity : AppCompatActivity() {
         with(binding.webview){
             addJavascriptInterface(WebViewInterface(this@WebViewActivity), "MyScript")
             if (isCreateNew){
+                clearHistory()
+                clearFormData()
+                clearCache(true)
+                CookieManager.getInstance().removeAllCookies(null)
+                CookieManager.getInstance().removeSessionCookies(null)
+                CookieManager.getInstance().flush()
                 WebStorage.getInstance().deleteAllData()
 
             }
             loadUrl("https://readyplayer.me/avatar")
         }
-        // TODO, implement custom loader
     }
 }
