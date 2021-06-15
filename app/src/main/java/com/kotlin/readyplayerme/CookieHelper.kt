5a31dc23a4d0982cef2a65698c90d215bd0a37b0
ihfazhillah <mihfazhillah@gmail.com>
2021-06-15 20:25:09 +0700

    change key as private constants

--- app/src/main/java/com/kotlin/readyplayerme/CookieHelper.kt
+++ app/src/main/java/com/kotlin/readyplayerme/CookieHelper.kt
@@ -5,8 +5,12 @@ import android.content.Context
 class CookieHelper(context: Context) {
     private val preference = context.getSharedPreferences("PlayerMe", Context.MODE_PRIVATE)
 
-    fun getUpdateState() = preference.getBoolean("hasCookie", false)
+    fun getUpdateState() = preference.getBoolean(HAS_COOKIE, false)
     fun setUpdateState(state: Boolean){
-        preference.edit().putBoolean("hasCookie", state).apply()
+        preference.edit().putBoolean(HAS_COOKIE, state).apply()
+    }
+
+    companion object {
+        private const val HAS_COOKIE = "hasCookie"
     }
 }
