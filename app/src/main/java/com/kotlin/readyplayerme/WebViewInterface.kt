155047c1f794f480840d56278b34d7e029fca0be
Sercan Altundas <sercan.altundas@gmail.com>
2022-03-03 18:00:22 +0200

    feat(webview): parse json if post message 2

--- app/src/main/java/com/kotlin/readyplayerme/WebViewInterface.kt
+++ app/src/main/java/com/kotlin/readyplayerme/WebViewInterface.kt
@@ -7,20 +7,35 @@ import android.content.Context
 import android.content.Intent
 import android.webkit.JavascriptInterface
 import android.widget.Toast
+import org.json.JSONObject
 
 class WebViewInterface(private val context: Context) {
 
     @JavascriptInterface
     fun receiveData(text: String){
-        // add to clipboard
-        val data = ClipData.newPlainText("Ready Player Me", text)
+        // extract avatar url from received message
+        var url = ""
+
+        if(text.endsWith(".glb"))   // post message v1, will be deprecated
+        {
+            url = text;
+        }
+        else    // post message v2
+        {
+            val json = JSONObject(text);
+            url = json.getJSONObject("data").getString("url");
+        }
+
+        // copy to clipboard
+        val data = ClipData.newPlainText("Ready Player Me", url)
         val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         clipboardManager.setPrimaryClip(data)
         Toast.makeText(context, "Url copied into clipboard.", Toast.LENGTH_SHORT).show()
 
+        // display modal window with the avatar url
         val builder = AlertDialog.Builder(context).apply {
             setTitle("Result")
-            setMessage(text)
+            setMessage(url)
             setPositiveButton("Ok"){ dialog, _ ->
                 dialog.dismiss()
                 context.startActivity(
